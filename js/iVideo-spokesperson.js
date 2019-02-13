c8856f2ae548a237bf61d5d7f4189c90af31f8a5
Andy Knudsen <andy@websitetalkingheads.com>
2019-02-12 17:57:38 -0700

    working for poster and actor

--- js/iVideo-spokesperson.js
+++ js/iVideo-spokesperson.js
@@ -1,8 +1,6 @@
 // JavaScript Document
 $(document).ready(function () {
 	var holder = $("#talking-heads-video_html5_api");
-	holder.width("540px");
-	holder.height("360px");
 	var video = $("#talking-heads-video_html5_api")[0];
 	var srcBase = "https://www.websitetalkingheads.com/";
 	var name, alt, srcVideo, formLeft;
@@ -15,7 +13,22 @@ $(document).ready(function () {
 			alt = " - " + $(this).attr("alt");
 		}
 		formLeft = "111px";
+		holder.width("540px");
+		holder.height("360px");
 		showVideo();
+	});
+	$(".poster").click(function () {
+		name = $(this).attr("data-video");
+		srcVideo = srcBase + "ivideo/videos/" + name + ".mp4";
+		if (!$(this).attr("alt")) {
+			alt = "";
+		} else {
+			alt = " - " + $(this).attr("alt");
+		}
+		formLeft = "240px";
+		showVideo();
+		holder.width("768px");
+		holder.height("432px");
 	});
 
 	function showVideo() {
@@ -25,10 +38,9 @@ $(document).ready(function () {
 		video.play();
 	}
 	$('#contact').click(function () {
-		console.log("click");
 		video.pause();
 		$('#form').addClass('d-block');
-		$('#form').css("left",formLeft);
+		$('#form').css("left", formLeft);
 	});
 	$('#contactClose').click(function () {
 		$('#form').removeClass('d-block');
