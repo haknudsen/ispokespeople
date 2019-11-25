ce07390ea2d3b4c463724aebd05231a13b070375
Andy Knudsen <paris_giovanni@yahoo.com>
2019-11-25 16:27:11 -0700

    added Order page

---
+++ order/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+<title>iSpokespeople Video Spokespeople</title>
+<meta name="description" content="At iSpokespeople We pride ourselves on our high quality, successful spokespeople We have the BEST in the business.">
+<meta name="keywords" content="Spokespeople, Video Spokesperson, Virtual Spokesperson, Website Spokesperson, Web Spokesperson, Spokesmodel, Walk On Actor,Virtual Actor, Person on Website, Online Spokesperson.">
+<?php include("../includes/head.php"); ?>
+</head>
+<?php include("../includes/nav.php"); ?>
+<section class="alert alert-info">
+  <div class="container py-3">
+    <div class="row">
+      <div class="col-lg-8 col-sm-6">
+        <div class="order-steps">
+          <h2 class="text-center">Steps to Order Your iSpokesperson</h2>
+          <ol>
+            <li><a href="../spokespeople/" title="Our Spokespeople" target="new">Choose Your Spokesperson <i class="far fa-user"></i></a></li>
+            <li>Choose the length of your video
+              <ul>
+                <li>30 Seconds: Up to 90 Words</li>
+                <li>60 Seconds 91 to 180 Words</li>
+                <li>90 Seconds 181 to 270 Words</li>
+              </ul>
+            </li>
+            <li>Video Crop-How your iSpokesperson is Framed</li>
+            <li>Position on Screen- Where your iSpokesperson appears on screen</li>
+            <li>How your iSpokesperson first appears</li>
+            <li>Autostart</li>
+            <li>How often the video plays when a person visits your site</li>
+            <li>Enter Your Script- You Will be able to update this later</li>
+          </ol>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-152889173" itemscope itemtype="http://schema.org/Product" data-single-product-id="152889173">
+          <div itemprop="image"></div>
+          <div class="ecwid-title" itemprop="name" content="Website Spokesperson"></div>
+          <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+            <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button">
+              <div itemprop="priceCurrency" content="USD"></div>
+            </div>
+          </div>
+          <div customprop="options"></div>
+          <div customprop="addtobag"></div>
+        </div>
+        <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+      </div>
+    </div>
+  </div>
+</section>
+<?php include("../includes/footer.php"); ?>
+</body></html>
