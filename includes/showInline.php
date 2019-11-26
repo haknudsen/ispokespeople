94ee23ae059f52e061b7932df68ded25a0371e4e
Andy Knudsen <andy@websitetalkingheads.com>
2019-11-26 15:29:31 -0700

    connect

--- includes/showInline.php
+++ includes/showInline.php
@@ -16,6 +16,13 @@ if ( !$btnColor ) {
 }
 echo PHP_EOL;
 echo '<div class="embed-responsive embed-responsive-16by9">
+<div itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+<div itemprop="name">' . $video . '</div>
+<meta itemprop="duration" content="T1M33S" />
+<meta itemprop="thumbnailUrl" content="https://www.websitetalkingheads.com/ivideo/videos/' . $video . '.jpg"" />
+<meta itemprop="contentURL" content="https://www.websitetalkingheads.com/ivideo/videos/' . $video . '.mp4" />
+<meta itemprop="embedURL" content="http://www.example.com/videoplayer.swf?video=123" />
+<meta itemprop="uploadDate" content="2018-23-11T08:00:00+08:00" />
 	<video style="background-color:'. $bgColor .'" class="video-js embed-responsive-item" controls preload="auto" poster="https://www.websitetalkingheads.com/ivideo/videos/' . $video . '.jpg" data-setup="{}">
 		<source src="https://www.websitetalkingheads.com/ivideo/videos/' . $video . '.mp4" type="video/mp4">
 		<p class="vjs-no-js">
@@ -24,32 +31,6 @@ echo '<div class="embed-responsive embed-responsive-16by9">
 		</p>
 	</video>
 </div>
-';
-echo PHP_EOL;
-echo '<script type="application/ld+json">
-	{
-		"@context": "https://schema.org",
-		"@type": "VideoObject",
-		"name": "' . $video . '",
-		"description": "' . $alt . '",
-		"thumbnailUrl": "https://www.websitetalkingheads.com/ivideo/videos/' . $video . '.jpg",
-		"uploadDate": "2018-11-31T08:00:00+08:00",
-		"duration": "PT1M54S",
-		"publisher": {
-			"@type": "Organization",
-			"name": "Website Talking Heads",
-			"logo": {
-				"@type": "ImageObject",
-				"url": "https://www.websitetalkingheads.com/images/Talking_Heads_Banner_Logo.png",
-				"width": 247,
-				"height": 100
-			}
-		},
-		"contentUrl": "https://www.websitetalkingheads.com/ivideo/videos/' . $video . '.mp4",
-		"embedUrl": "https://www.websitetalkingheads.com/ivideo/videos/' . $video . '",
-		"interactionCount": "7018"
-	}
-</script>
 ';
 
 ?>
