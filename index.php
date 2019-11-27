ea241381229c6a1e1257ace1538fe675223099b2
Andy Knudsen <andy@websitetalkingheads.com>
2019-11-26 17:17:31 -0700

    fixing up home page

--- index.php
+++ index.php
@@ -2,133 +2,126 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<meta name="description" content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Choose from our wide selection of Video Spokespeople.">
-<script defer src="https://pro.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-DtPgXIYsUR6lLmJK14ZNUi11aAoezQtw4ut26Zwy9/6QXHH8W3+gjrRDT+lHiiW4" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link href="css/custom.css" rel="stylesheet" type="text/css">
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <title>iSpokespeople&reg;|Video Spokespeople</title>
+<meta name="description" content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Choose from our wide selection of Video Spokespeople.">
+<?php include("includes/head-home.php"); ?>
 </head>
 
 <body>
 <?php include("includes/nav.php"); ?>
 <section class="container text-center">
-  <h1>The Best in Video Spokespeople</h1>
-  <h2>We Offer 4 Kinds of Spokesperson Videos</h2>
+	<h1>The Best in Video Spokespeople</h1>
+	<h2>We Offer 4 Kinds of Spokesperson Videos</h2>
 </section>
 <section class="container-fluid mt-2 clearfix">
 <div class="row">
 <div class="container card-group my-3" id="spokesperson_videos">
-  <div class="card card-outline-info">
-    <div class="card-block"> <img class="card-img-top" src="https://websitetalkingheads.com/images/WebsiteVideo.gif" alt="Custom Video Player" title="Custom Video Player"/>
-      <h3 class="card-title">#1 <i>Website Spokesperson</i></h3>
-      <article>Add a Spokesperson to your website to welcome visitors and direct them to key areas. Increase conversion rates and boost leads. Uses Flash for Transparent effect, but when detects mobile device uses standard MP4.</article>
-    </div>
-  </div>
-  <div class="card card-outline-info">
-    <div class="card-block"> <img class="card-img-top" src="https://websitetalkingheads.com/images/YouTubeReady.gif" title="YouTube Ready Video" alt="YouTube Ready Video"/>
-      <h3 class="card-title">#2 with Logo</h3>
-      <article> A powerful YouTube and Facebook compatible sales video. Choose an actor, send your script and logo. We put it all together and provide you a Full HD 1920X1080 video in MP4 format. Upload it to YouTube, Twitter, Instagram, etc...</article>
-    </div>
-  </div>
-  <div class="card card-outline-info">
-    <div class="card-block"> <img class="card-img-top" src="https://websitetalkingheads.com/images/GreenScreenVideo.gif" alt="Green Screen Video" title="Green Screen Video"/>
-      <h3 class="card-title">#3 Green Screen File</h3>
-      <article> Attention Marketing and Ad agencies, professional editors and compositiors: Save time and HUNDREDS of dollars, We provide you with a hi-res file to use in your Internet video project.</article>
-    </div>
-  </div>
+	<div class="card card-outline-info">
+		<div class="card-body"> <img class="card-img-top" src="https://websitetalkingheads.com/images/WebsiteVideo.gif" alt="Custom Video Player" title="Custom Video Player"/>
+			<h3 class="card-title">#1 <i>Website Spokesperson</i></h3>
+			<article>Add a Spokesperson to your website to welcome visitors and direct them to key areas. Increase conversion rates and boost leads. Uses Flash for Transparent effect, but when detects mobile device uses standard MP4.</article>
+		</div>
+	</div>
+	<div class="card card-outline-info">
+		<div class="card-body"> <img class="card-img-top" src="https://websitetalkingheads.com/images/YouTubeReady.gif" title="YouTube Ready Video" alt="YouTube Ready Video"/>
+			<h3 class="card-title">#2 with Logo</h3>
+			<article> A powerful YouTube and Facebook compatible sales video. Choose an actor, send your script and logo. We put it all together and provide you a Full HD 1920X1080 video in MP4 format. Upload it to YouTube, Twitter, Instagram, etc...</article>
+		</div>
+	</div>
+	<div class="card card-outline-info">
+		<div class="card-body"> <img class="card-img-top" src="https://websitetalkingheads.com/images/GreenScreenVideo.gif" alt="Green Screen Video" title="Green Screen Video"/>
+			<h3 class="card-title">#3 Green Screen File</h3>
+			<article> Attention Marketing and Ad agencies, professional editors and compositiors: Save time and HUNDREDS of dollars, We provide you with a hi-res file to use in your Internet video project.</article>
+		</div>
+	</div>
 </div>
 </section>
 <section class="container">
-  <div class="card card-outline-info">
-    <div class="card-block">
-      <div class="card-solo"><img class="card-img-top" src="http://img.youtube.com/vi/ozwgqkGubFo/mqdefault.jpg" id="Quote Zoom" title="Video Presentation - Quote Zoom" title="Video Presentation - Quote Zoom" alt="Video Presentation - Quote Zoom"> </div>
-      <h3 class="card-title">#4 Custom Video Presentation</h3>
-      <article> With our Custom <a href="Video-Presentations/">Video Presentations</a> you get a Video Spokesperson, Professional Editing, Motion Graphics, and Lots of Images and Effects. Great for your Corporate or Sales Video, a Product Demonstration or Demonstration of your Website or Software.</article>
-    </div>
-  </div>
-  </div>
+	<div class="card card-outline-info card-solo">
+		<div class="card-img-top">
+			<?php $video="Video%20Presentation%20Show%20Reel"; include("includes/showInline.php"); ?>
+		</div>
+		<div class="card-body">
+			<h3 class="card-title">#4 Custom Video Presentation</h3>
+			<article> With our Custom <a href="Video-Presentations/">Video Presentations</a> you get a Video Spokesperson, Professional Editing, Motion Graphics, and Lots of Images and Effects. Great for your Corporate or Sales Video, a Product Demonstration or Demonstration of your Website or Software.</article>
+		</div>
+	</div>
 </section>
 <section class="alert alert-info mt-3">
-  <div class="container">
-    <h3 class="text-center">What a iSpokesperson Can Do For You</h3>
-    <blockquote>A iSpokesperson will greet your visitors to help them feel more comfortable at your website. Your iSpokesperson can build confidence that your business is solid by giving your business a professional face and clarify the important parts of your website and help the visitor to understand how your products and services will benefit them.</blockquote>
-  </div>
+	<div class="container">
+		<h3 class="text-center">What a iSpokesperson Can Do For You</h3>
+		<blockquote>A iSpokesperson will greet your visitors to help them feel more comfortable at your website. Your iSpokesperson can build confidence that your business is solid by giving your business a professional face and clarify the important parts of your website and help the visitor to understand how your products and services will benefit them.</blockquote>
+	</div>
 </section>
 <section class="alert alert-info">
-  <div class="card-group my-3" id="awards">
-    <div class="card"> <a href="#" data-toggle="tooltip" title="#1 in Video Production"> <img class="card-img-top seal d-block" src="https://websitetalkingheads.com/images/video-production-seal.png"  alt="Best in Video Production"  title="Best in Video Production" /> </a>
-      <div class="card-block award">
-        <h2 class="card-title">#1 in Video Production</h2>
-        <h5 class="card-subtitle">Best Production Quality</h5>
-        <article>
-          <p class="card-text">When for the right video production company, you need somebody who can reliably get a handle on your message and convey your message. Each and every part of your video from Talking Heads&reg; will be centered around your message.</p>
-          <p class="card-text">Our videos are completely filmed and composited right here at our studio to maintain our high quality standards and accuracy. We do not outsource our tasks to other workshops. Every little thing is totally filmed RIGHT HERE!</p>
-        </article>
-      </div>
-      <!-- card-block -->
-      <div class="card-footer text-center"> <a class="card-link" href="about/">About Us</a> </div>
-    </div>
-    <div class="card">
-      <div class="mb-5 mt-5"> <a href="#" data-toggle="tooltip" title="Winner Best Web Design Agencies - Video Production"><img class="card-img-top seal d-block mb-1" title="Winner in Video Production"   alt="Winner in Video Production" src="https://www.websitetalkingheads.com/images/video-production-winner.png"></a> </div>
-      <div class="card-block award">
-        <h2 class="card-title">Best<br>
-          Video Production</h2>
-        <h5 class="card-subtitle">Engaging Video</h5>
-        <article>
-          <p class="card-text">Our Award Winning Video Presentations are all filmed and put together at our studio. We don't use freelancers or temps.</p>
-          <p class="card-text">We create video presentations that enhance your essential vision without breaking your bank. Instead of spending thousands or tens of thousands on a "viral video", we recommend making videos that work for all of you day, consistently passing on a solid message to potential customers.</p>
-        </article>
-      </div>
-      <!-- card-block -->
-      <div class="card-footer text-center"> <a href="Video-Presentations/" title="Video Presentations" class="card-link">Video Presentations</a> </div>
-    </div>
-    <div class="card"> <a href="http://www.topseos.com/rankings-of-best-virtual-spokesperson-companies" data-toggle="tooltip" title="#1 in Video Spokesperson"><img class="card-img-top seal d-block" src="https://websitetalkingheads.com/images/video-spokesperson-seal.png"  title="Best in Video Spokesperson"  alt="Best in Video Spokesperson"  /></a>
-      <div class="card-block award">
-        <h2 class="card-title">#1 Video Spokespeople</h2>
-        <h5 class="card-subtitle">Best Quality Spokespeople</h5>
-        <article>
-          <p class="card-text">We have a wide variety of Spokespeople that encompass different styles of articulating your message. This guarantees you will be able to discover a Spokesperson who can best deliver your message and vision to your target audience.</p>
-          <p>Our Website Spokespeople can easily present information, news updates, notifications or even show website visitors around your web site. Their task might consist of keeping your website visitor's interest as well as entice them to purchase your product or service.</p>
-        </article>
-      </div>
-      <div class="card-footer text-center"> <a class="card-link" href="spokespeople/">Our Spokespeople</a> </div>
-    </div>
-  </div>
+	<div class="card-group my-3" id="awards">
+		<div class="card">
+			<h2 class=" card-header">#1 in Video Production</h2>
+			<a href="#" data-toggle="tooltip" title="#1 in Video Production"> <img class="card-img-top seal d-block" src="https://websitetalkingheads.com/images/video-production-seal.png"  alt="Best in Video Production"  title="Best in Video Production" /> </a>
+			<div class="card-body award">
+				<h5 class="card-title">Best Production Quality</h5>
+				<article>
+					<p class="card-text">When for the right video production company, you need somebody who can reliably get a handle on your message and convey your message. Each and every part of your video from Talking Heads&reg; will be centered around your message.</p>
+					<p class="card-text">Our videos are completely filmed and composited right here at our studio to maintain our high quality standards and accuracy. We do not outsource our tasks to other workshops. Every little thing is totally filmed RIGHT HERE!</p>
+				</article>
+			</div>
+			<!-- card-body -->
+			<div class="card-footer text-center"> <a class="card-link" href="about/">About Us</a> </div>
+		</div>
+		<div class="card">
+			<h2 class=" card-header">Best Video Production</h2>
+			<div class="mb-5 mt-5"> <a href="#" data-toggle="tooltip" title="Winner Best Web Design Agencies - Video Production"><img class="card-img-top seal d-block mb-1" title="Winner in Video Production"   alt="Winner in Video Production" src="https://www.websitetalkingheads.com/images/video-production-winner.png"></a> </div>
+			<div class="card-body award">
+				<h5 class="card-title">Engaging Video</h5>
+				<article>
+					<p class="card-text">Our Award Winning Video Presentations are all filmed and put together at our studio. We don't use freelancers or temps.</p>
+					<p class="card-text">We create video presentations that enhance your essential vision without breaking your bank. Instead of spending thousands or tens of thousands on a "viral video", we recommend making videos that work for all of you day, consistently passing on a solid message to potential customers.</p>
+				</article>
+			</div>
+			<!-- card-body -->
+			<div class="card-footer text-center"> <a href="Video-Presentations/" title="Video Presentations" class="card-link">Video Presentations</a> </div>
+		</div>
+		<div class="card">
+			<h2 class=" card-header">#1 Video Spokespeople</h2>
+			<a href="http://www.topseos.com/rankings-of-best-virtual-spokesperson-companies" data-toggle="tooltip" title="#1 in Video Spokesperson"><img class="card-img-top seal d-block" src="https://websitetalkingheads.com/images/video-spokesperson-seal.png"  title="Best in Video Spokesperson"  alt="Best in Video Spokesperson"  /></a>
+			<div class="card-body award">
+				<h5 class="card-title">Best Quality Spokespeople</h5>
+				<article>
+					<p class="card-text">We have a wide variety of Spokespeople that encompass different styles of articulating your message. This guarantees you will be able to discover a Spokesperson who can best deliver your message and vision to your target audience.</p>
+					<p>Our Website Spokespeople can easily present information, news updates, notifications or even show website visitors around your web site. Their task might consist of keeping your website visitor's interest as well as entice them to purchase your product or service.</p>
+				</article>
+			</div>
+			<div class="card-footer text-center"> <a class="card-link" href="spokespeople/">Our Spokespeople</a> </div>
+		</div>
+	</div>
 </section>
 <section class="container">
-  <div class="card">
-    <div class="card-header text-center">
-      <h2 class="card-title"><strong>Web Video</strong> Blog</h2>
-      <h3 id="title-marketing"></h3>
-    </div>
-    <div class="card-body">
-      <p id="excerpt-marketing" class="text-info"></p>
-    </div>
-    <div class="card-footer">
-      <div class="m-auto"> <img id="img-marketing" src="http://bestwhiteboardvideo.com/wp-content/uploads/wp_image_duplicator_uploads/Live Spokespeople.jpg" class="img-fluid rounded mx-auto d-block" alt="Our Spokespeople"/> </div>
-    </div>
-  </div>
+	<div class="card">
+		<div class="card-header text-center">
+			<h2 class="card-title"><strong>Web Video</strong> Blog</h2>
+			<h3 id="title-marketing"></h3>
+		</div>
+		<div class="card-body">
+			<p id="excerpt-marketing" class="text-info"></p>
+		</div>
+		<div class="card-footer">
+			<div class="m-auto"> <img id="img-marketing" src="http://bestwhiteboardvideo.com/wp-content/uploads/wp_image_duplicator_uploads/Live Spokespeople.jpg" class="img-fluid rounded mx-auto d-block" alt="Our Spokespeople"/> </div>
+		</div>
+	</div>
 </section>
 <?php include("includes/footer.php"); ?>
 
 <!-- content container -->
 <section class="coupon">
-<div id="embed-scratchcard-5LpQX6aP1X"><link rel="stylesheet" href="//vidscratch.com/app/public/scratchcard.css"><script type="text/javascript" src="//vidscratch.com/app/public/jquery-3.4.1.min.js"></script><script type="text/javascript" id="cardjs-5LpQX6aP1X" src="//vidscratch.com/app/public/scratchcard.js"></script><iframe id="scratchcard-frame-5LpQX6aP1X" name="scratchcard-frame-5LpQX6aP1X" class="exit-popup scratchcardframe" scrolling="no" src="//vidscratch.com/app/api/embed-game/OTYy/MTI1NTE=/exit-popup" frameborder="0" allowfullscreen="" style="height: 360px;width:300px;display:none;"></iframe><div class="exit-popup-backcover" id="scratcard_backcover"></div></div>
+	<div id="embed-scratchcard-5LpQX6aP1X">
+		<link rel="stylesheet" href="//vidscratch.com/app/public/scratchcard.css">
+		<script type="text/javascript" src="//vidscratch.com/app/public/jquery-3.4.1.min.js"></script><script type="text/javascript" id="cardjs-5LpQX6aP1X" src="//vidscratch.com/app/public/scratchcard.js"></script>
+		<iframe id="scratchcard-frame-5LpQX6aP1X" name="scratchcard-frame-5LpQX6aP1X" class="exit-popup scratchcardframe" scrolling="no" src="//vidscratch.com/app/api/embed-game/OTYy/MTI1NTE=/exit-popup" frameborder="0" allowfullscreen="" style="height: 360px;width:300px;display:none;"></iframe>
+		<div class="exit-popup-backcover" id="scratcard_backcover"></div>
+	</div>
 </section>
-<script src="js/jquery.matchHeight.js"></script> 
-<script src="js/blog-marketing.js"></script> 
-<script>
-		$( document ).ready( function () {
-			$( '.award' ).matchHeight( {
-				byRow: false
-			} );
-		} );
-	</script>
+<script src="js/blog-marketing.js"></script>
 </body>
 </html>
