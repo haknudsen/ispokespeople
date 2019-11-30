178345203e212f654cb3d01f276337c1f9a0574f
Andy Knudsen <andy@websitetalkingheads.com>
2019-11-29 18:06:17 -0700

    working on index structure

--- index.php
+++ index.php
@@ -11,41 +11,30 @@
 
 <body>
 <?php include("includes/nav.php"); ?>
-<section class="container text-center">
-	<h1>The Best in Video Spokespeople</h1>
-	<h2>We Offer 4 Kinds of Spokesperson Videos</h2>
-</section>
-<section class="container-fluid mt-2 clearfix">
-<div class="row">
-<div class="container card-group my-3" id="spokesperson_videos">
-	<div class="card card-outline-info">
-		<div class="card-body"> <img class="card-img-top" src="https://websitetalkingheads.com/images/WebsiteVideo.gif" alt="Custom Video Player" title="Custom Video Player"/>
-			<h3 class="card-title">#1 <i>Website Spokesperson</i></h3>
-			<article>Add a Spokesperson to your website to welcome visitors and direct them to key areas. Increase conversion rates and boost leads. Uses Flash for Transparent effect, but when detects mobile device uses standard MP4.</article>
+<section class="container-fluid">
+	<h1 class="text-center">The Best in Video Spokespeople</h1>
+	<div class="card-group my-3" id="spokesperson_videos">
+		<div class="card card-outline-info">
+			<div class="card-img-top">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="https://www.websitetalkingheads.com/ivideo/talking-heads-player.php?video=Virtual Spokespeople&autostart=mute&controls=true"></iframe>
+				</div>
+			</div>
+			<div class="card-body">
+				<h3 class="card-title">Website <em>iSpokesperson</em></h3>
+				<article>Add a Spokesperson to your website to welcome visitors and direct them to key areas. Increase conversion rates and boost leads. Uses Flash for Transparent effect, but when detects mobile device uses standard MP4.</article>
+			</div>
 		</div>
-	</div>
-	<div class="card card-outline-info">
-		<div class="card-body"> <img class="card-img-top" src="https://websitetalkingheads.com/images/YouTubeReady.gif" title="YouTube Ready Video" alt="YouTube Ready Video"/>
-			<h3 class="card-title">#2 with Logo</h3>
-			<article> A powerful YouTube and Facebook compatible sales video. Choose an actor, send your script and logo. We put it all together and provide you a Full HD 1920X1080 video in MP4 format. Upload it to YouTube, Twitter, Instagram, etc...</article>
-		</div>
-	</div>
-	<div class="card card-outline-info">
-		<div class="card-body"> <img class="card-img-top" src="https://websitetalkingheads.com/images/GreenScreenVideo.gif" alt="Green Screen Video" title="Green Screen Video"/>
-			<h3 class="card-title">#3 Green Screen File</h3>
-			<article> Attention Marketing and Ad agencies, professional editors and compositiors: Save time and HUNDREDS of dollars, We provide you with a hi-res file to use in your Internet video project.</article>
-		</div>
-	</div>
-</div>
-</section>
-<section class="container">
-	<div class="card card-outline-info card-solo">
-		<div class="card-img-top">
-			<?php $video="Video%20Presentation%20Show%20Reel"; include("includes/showInline.php"); ?>
-		</div>
-		<div class="card-body">
-			<h3 class="card-title">#4 Custom Video Presentation</h3>
-			<article> With our Custom <a href="Video-Presentations/">Video Presentations</a> you get a Video Spokesperson, Professional Editing, Motion Graphics, and Lots of Images and Effects. Great for your Corporate or Sales Video, a Product Demonstration or Demonstration of your Website or Software.</article>
+		<div class="card card-outline-info">
+			<div class="card-img-top">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="https://www.websitetalkingheads.com/ivideo/talking-heads-player.php?video=Video%20Presentation%20Show%20Reel&autostart=mute&controls=true"></iframe>
+				</div>
+			</div>
+			<div class="card-body">
+				<h3 class="card-title"><em>iSpokesperson</em> Presentation</h3>
+				<article> With our Custom <a href="Video-Presentations/">Video Presentations</a> you get a Video Spokesperson, Professional Editing, Motion Graphics, and Lots of Images and Effects. Great for your Corporate or Sales Video, a Product Demonstration or Demonstration of your Website or Software.</article>
+			</div>
 		</div>
 	</div>
 </section>
@@ -53,6 +42,20 @@
 	<div class="container">
 		<h3 class="text-center">What a iSpokesperson Can Do For You</h3>
 		<blockquote>A iSpokesperson will greet your visitors to help them feel more comfortable at your website. Your iSpokesperson can build confidence that your business is solid by giving your business a professional face and clarify the important parts of your website and help the visitor to understand how your products and services will benefit them.</blockquote>
+	</div>
+</section>
+<section class="container mb-3">
+	<div class="card">
+		<div class="card-header text-center">
+			<h2 class="card-title"><strong>Web Video</strong> Blog</h2>
+			<h3 id="title-marketing"></h3>
+		</div>
+		<div class="card-body">
+			<p id="excerpt-marketing" class="text-info"></p>
+		</div>
+		<div class="card-footer">
+			<div class="m-auto"> <img id="img-marketing" src="http://bestwhiteboardvideo.com/wp-content/uploads/wp_image_duplicator_uploads/Live Spokespeople.jpg" class="img-fluid rounded mx-auto d-block" alt="Our Spokespeople"/> </div>
+		</div>
 	</div>
 </section>
 <section class="alert alert-info">
@@ -97,28 +100,14 @@
 		</div>
 	</div>
 </section>
-<section class="container">
-	<div class="card">
-		<div class="card-header text-center">
-			<h2 class="card-title"><strong>Web Video</strong> Blog</h2>
-			<h3 id="title-marketing"></h3>
-		</div>
-		<div class="card-body">
-			<p id="excerpt-marketing" class="text-info"></p>
-		</div>
-		<div class="card-footer">
-			<div class="m-auto"> <img id="img-marketing" src="http://bestwhiteboardvideo.com/wp-content/uploads/wp_image_duplicator_uploads/Live Spokespeople.jpg" class="img-fluid rounded mx-auto d-block" alt="Our Spokespeople"/> </div>
-		</div>
-	</div>
-</section>
 <?php include("includes/footer.php"); ?>
 
 <!-- content container -->
 <section class="coupon">
-	<div id="embed-scratchcard-5LpQX6aP1X">
+	<div id="embed-scratchcard-9Bx1NQFdAj">
 		<link rel="stylesheet" href="//vidscratch.com/app/public/scratchcard.css">
-		<script type="text/javascript" src="//vidscratch.com/app/public/jquery-3.4.1.min.js"></script><script type="text/javascript" id="cardjs-5LpQX6aP1X" src="//vidscratch.com/app/public/scratchcard.js"></script>
-		<iframe id="scratchcard-frame-5LpQX6aP1X" name="scratchcard-frame-5LpQX6aP1X" class="exit-popup scratchcardframe" scrolling="no" src="//vidscratch.com/app/api/embed-game/OTYy/MTI1NTE=/exit-popup" frameborder="0" allowfullscreen="" style="height: 360px;width:300px;display:none;"></iframe>
+		<script type="text/javascript" src="//vidscratch.com/app/public/jquery-3.4.1.min.js"></script><script type="text/javascript" id="cardjs-9Bx1NQFdAj" src="//vidscratch.com/app/public/scratchcard.js"></script>
+		<iframe id="scratchcard-frame-9Bx1NQFdAj" name="scratchcard-frame-9Bx1NQFdAj" class="exit-popup scratchcardframe" scrolling="no" src="//vidscratch.com/app/api/embed-game/OTYy/MTMxMTk=/exit-popup" frameborder="0" allowfullscreen="" style="height: 380px;width:300px;display:none;"></iframe>
 		<div class="exit-popup-backcover" id="scratcard_backcover"></div>
 	</div>
 </section>
